31d5a80ddc5f45e0b5f32a036d9a9a7ce0b6bb8b
JPatrik <jokhel.roland@gmail.com>
2025-11-25 17:52:03 +0100

    feat: solve day02 part2

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -11,6 +11,10 @@ fn main() {
         Ok(res) => println!("The part1 solution is: {}", res),
         Err(e) => eprintln!("Part1 Error {}", e),
     }
+    match run_part2(&input) {
+        Ok(res) => println!("The part2 solution is: {}", res),
+        Err(e) => eprintln!("Part2 Error {}", e),
+    }
 }
 
 fn run_part1(input: &str) -> Result<i32, Box<dyn Error>> {
@@ -50,6 +54,51 @@ fn run_part1(input: &str) -> Result<i32, Box<dyn Error>> {
     return Ok(result);
 }
 
+fn run_part2(input: &str) -> Result<String, Box<dyn Error>> {
+    let numbers: HashMap<(i32, i32), &str> = HashMap::from([
+        ((0, 2), "1"),
+        ((-1, 1), "2"),
+        ((0, 1), "3"),
+        ((1, 1), "4"),
+        ((-2, 0), "5"),
+        ((-1, 0), "6"),
+        ((0, 0), "7"),
+        ((1, 0), "8"),
+        ((2, 0), "9"),
+        ((-1, -1), "A"),
+        ((0, -1), "B"),
+        ((1, -1), "C"),
+        ((0, -2), "D"),
+    ]);
+
+    let mut position: (i32, i32) = (-2, 0);
+    let mut num_str = String::new();
+
+    for line in input.lines() {
+        for c in line.chars() {
+            let mut new_position = position.clone();
+            match c {
+                'U' => new_position.1 = new_position.1 + 1,
+                'D' => new_position.1 = new_position.1 - 1,
+                'L' => new_position.0 = new_position.0 - 1,
+                'R' => new_position.0 = new_position.0 + 1,
+                _ => return Err("invalid character received".into()),
+            }
+
+            if numbers.contains_key(&new_position) {
+                position = new_position
+            }
+        }
+
+        match numbers.get(&position) {
+            Some(n) => num_str += n,
+            None => return Err(format!("position is not in numbers: {:?}", position).into()),
+        }
+    }
+
+    return Ok(num_str.to_string());
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -75,5 +124,15 @@ RRDDD
 LURDL
 UUUUD",
         1985
+    );
+    test_run!(example_3, run_part2, "ULL", "5");
+    test_run!(
+        example_4,
+        run_part2,
+        "ULL
+RRDDD
+LURDL
+UUUUD",
+        "5DB3"
     );
 }
